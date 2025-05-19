e96630900f2172e71319afddf11320f782cb3014
gpguestpost <106247914+gpguestpost@users.noreply.github.com>
2025-05-19 18:34:00 +0500

    Add files via upload

---
+++ Lab 8/Method Overloading/Task1/Main.java
@@ -0,0 +1,14 @@
+/* @author nisar */
+package Task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculation calc = new Calculation();
+
+        System.out.println(calc.add(5, 3));
+        System.out.println(calc.add(2.5, 3.7));
+        System.out.println(calc.add("Nisar", "Ahmed"));
+
+    }
+}
